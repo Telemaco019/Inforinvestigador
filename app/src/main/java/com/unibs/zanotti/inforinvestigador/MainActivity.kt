926ae8191eec0647c1829f21169b7319cbabb41b
Michele Zanotti <m.zanotti@myti.it>
2019-03-30 11:49:01 +0100

    Added bottom navigation and top-toolbar

--- app/src/main/java/com/unibs/zanotti/inforinvestigador/MainActivity.kt
+++ app/src/main/java/com/unibs/zanotti/inforinvestigador/MainActivity.kt
@@ -1,12 +1,66 @@
 package com.unibs.zanotti.inforinvestigador
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import com.unibs.zanotti.inforinvestigador.fragment.HomeFragment
+import com.unibs.zanotti.inforinvestigador.fragment.LibraryFragment
+import com.unibs.zanotti.inforinvestigador.fragment.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set the toolbar layout as toolbar
+        val toolbar = findViewById(R.id.top_bar) as Toolbar
+        setSupportActionBar(toolbar)
+        // Set the title of the support action bar
+        supportActionBar?.setTitle(R.string.app_name)
+        supportActionBar?.setSubtitle(R.string.app_name)
+
+        val fragmentManager = supportFragmentManager
+
+        // Navigation fragments
+        val fragmentHome = HomeFragment()
+        val fragmentLibrary = LibraryFragment()
+        val fragmentProfile = ProfileFragment()
+
+        val navigationBar = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
+        navigationBar.setOnNavigationItemSelectedListener { item ->
+            val fragment: Fragment
+            when (item.itemId) {
+                R.id.bottom_bar_action_home -> {
+                    fragment = fragmentHome
+                }
+                R.id.bottom_bar_action_library -> {
+                    fragment = fragmentLibrary
+                }
+                R.id.bottom_bar_action_profile -> {
+                    fragment = fragmentProfile
+                }
+                else -> {
+                    fragment = fragmentHome
+                }
+            }
+
+            fragmentManager.beginTransaction().replace(R.id.fragment_placheholder, fragment).commit()
+            return@setOnNavigationItemSelectedListener true
+        }
+
+        // Set default selected fragment
+        navigationBar.selectedItemId = R.id.bottom_bar_action_home
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        // Put the item of the menu in the toolbar. Material Design is applied automatically
+        inflater.inflate(R.menu.menu_top_bar, menu)
+
+        return true
     }
 }
