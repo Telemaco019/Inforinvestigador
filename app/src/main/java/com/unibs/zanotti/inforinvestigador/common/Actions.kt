5b4a5643e215e77329bebd3b9ded52b8bb1a347f
Michele Zanotti <m.zanotti@myti.it>
2019-04-06 19:23:00 +0200

    Revert "Merge branch 'develop'"
    This reverts commit efd011272632aa850d7e6ead747c62e7b83f1693.

--- app/src/main/java/com/unibs/zanotti/inforinvestigador/common/Actions.kt
+++
@@ -1,7 +0,0 @@
-package com.unibs.zanotti.inforinvestigador.common
-
-class Actions {
-    companion object {
-        const val SHOW_PAPER = "com.unibs.zanotti.inforinvestigador.action.SHOW_PAPER"
-    }
-}
