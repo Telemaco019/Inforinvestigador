09b3a076ef04c261b93ac9ce6065f18ad2352039
Michele Zanotti <m.zanotti@myti.it>
2019-04-07 15:22:41 +0200

    Added collapsing toolbar

--- app/src/main/java/com/unibs/zanotti/inforinvestigador/common/ViewPaperActivity.kt
+++ app/src/main/java/com/unibs/zanotti/inforinvestigador/common/ViewPaperActivity.kt
@@ -2,17 +2,12 @@ package com.unibs.zanotti.inforinvestigador.common
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
 import com.unibs.zanotti.inforinvestigador.R
-import com.unibs.zanotti.inforinvestigador.common.model.Paper
 
 class ViewPaperActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_paper)
-
-        val text = findViewById<TextView>(R.id.test)
-        text.text = intent.getStringExtra(Paper.PAPER_ID_EXTRA)
     }
 }
