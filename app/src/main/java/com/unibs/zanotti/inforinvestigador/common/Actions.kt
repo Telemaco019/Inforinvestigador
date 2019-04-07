c8b9adb1195540b1bc175f5c97fbbc5ec603194f
Michele Zanotti <m.zanotti@myti.it>
2019-04-07 11:24:49 +0200

    Updated

--- app/src/main/java/com/unibs/zanotti/inforinvestigador/common/Actions.kt
+++ app/src/main/java/com/unibs/zanotti/inforinvestigador/common/Actions.kt
@@ -3,5 +3,6 @@ package com.unibs.zanotti.inforinvestigador.common
 class Actions {
     companion object {
         const val SHOW_PAPER = "com.unibs.zanotti.inforinvestigador.action.SHOW_PAPER"
+
     }
 }
