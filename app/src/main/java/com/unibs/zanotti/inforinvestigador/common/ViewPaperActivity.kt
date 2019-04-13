ee11b0c62b250a01e4ddaf073285b186629cdde8
Michele Zanotti <m.zanotti@myti.it>
2019-04-13 16:04:27 +0200

    Added back home item in the view paper toolbar

--- app/src/main/java/com/unibs/zanotti/inforinvestigador/common/ViewPaperActivity.kt
+++ app/src/main/java/com/unibs/zanotti/inforinvestigador/common/ViewPaperActivity.kt
@@ -4,20 +4,37 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import android.view.MenuItem
 import com.unibs.zanotti.inforinvestigador.R
+
 
 class ViewPaperActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_paper)
+        setupSupportActionBar()
+    }
 
+    private fun setupSupportActionBar() {
         val toolbar = findViewById<Toolbar>(R.id.top_bar)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_add_black_24dp)
 
+        // Show back icon
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Hide app title
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        // If the home item has been clicked (e.g. the back arrow), then close this activity
+        if (id == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
