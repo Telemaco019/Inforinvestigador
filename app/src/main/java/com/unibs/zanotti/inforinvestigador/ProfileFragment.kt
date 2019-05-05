5fe6b353d95795faea4a8d34cea715bca343121c
Michele Zanotti <m.zanotti@myti.it>
2019-05-05 10:31:45 +0200

    Added base design, fixed layout main Activity (margin from bottom nav)

--- app/src/main/java/com/unibs/zanotti/inforinvestigador/ProfileFragment.kt
+++ app/src/main/java/com/unibs/zanotti/inforinvestigador/ProfileFragment.kt
@@ -1,15 +1,11 @@
 package com.unibs.zanotti.inforinvestigador
 
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.unibs.zanotti.inforinvestigador.auth.LoginActivity
-import com.unibs.zanotti.inforinvestigador.utils.FirebaseUtils
 
 
 /**
@@ -24,7 +20,7 @@ class ProfileFragment : androidx.fragment.app.Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        val logoutButton = view.findViewById<Button>(R.id.logout_button)
+     /*   val logoutButton = view.findViewById<Button>(R.id.logout_button)
         logoutButton.setOnClickListener {
             val intent = Intent(context, LoginActivity::class.java)
             intent.putExtra(LoginActivity.BOOLEAN_EXTRA_DO_LOGOUT, true)
@@ -36,7 +32,7 @@ class ProfileFragment : androidx.fragment.app.Fragment() {
         createPaperButton.setOnClickListener {
             FirebaseUtils.populatePapersCollection()
         }
-
+*/
         return view
     }
 }
